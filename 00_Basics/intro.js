f48cd10d58edfbacfc0b665c14c0994c9209a893
TapanRoyal31 <tapanr3105@gmail.com>
2026-03-28 10:35:05 +0530

    basics

---
+++ 00_Basics/intro.js
@@ -0,0 +1,14 @@
+// var a;
+
+// console.log(typeof a);
+
+// // null --> 0x00
+// // object --> 000
+
+// let fn = typeof function(){
+//     console.log("hello");
+// }
+
+// console.log(fn);
+//--------------------------------------------------------------------------------------
+
