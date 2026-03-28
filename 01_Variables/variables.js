75b1da7bf56e4a185ae7db785cf76b14ccca7676
TapanRoyal31 <tapanr3105@gmail.com>
2026-03-28 11:52:55 +0530

    variable types

---
+++ 01_Variables/variables.js
@@ -0,0 +1,54 @@
+// var , let , const
+
+
+// var :- var global scope
+// scope : { } 
+
+// var greetings;
+
+// greetings = "good evening";
+// {
+//     let greetings = "Good morning"
+//     console.log(greetings);
+// }
+
+
+
+// console.log(greetings); // good evening
+
+// ----------------------------------- const ----------------------------------------------
+
+// const number = 32;
+
+// number = 33; // TypeError: Assignment to constant variable.
+
+// console.log(number);
+
+
+// ------------------------------------ output Prediction ---------------------------------
+
+
+/*
+    Unary Operator :- Single Operand and single operator
+                Ex. +a , -a 
+    Binary Operator :- double operands and double operators
+                Ex. a == b, a != b,.......
+*/
+
+// var a = '23';
+
+// var b = -a;
+// console.log(typeof b);
+
+// console.log(b);
+
+//  = --> assignment
+//  == --> comparison
+// === --> strict checking (datatype + value + memory location)
+
+// let a = '23'
+// let b = 23
+
+// console.log(a === b);
+
+
