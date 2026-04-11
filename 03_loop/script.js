72f1b1980ccc0114716987eb330830e714fd7a66
TapanRoyal31 <tapanr3105@gmail.com>
2026-04-11 11:17:22 +0530

    function task

--- 03_loop/script.js
+++ 03_loop/script.js
@@ -40,3 +40,40 @@
 //     console.log(str);
 //     str = '';
 // }
+
+
+// -------------------------------- Count Digit -----------------------------------------------
+/*
+    Task 6: Count Digits
+    Input: 987654
+    Expected Output: 6 
+*/
+
+// let number = 987654
+// let temp = number;
+// let count = 0;
+// while (temp > 0) {
+//     temp = parseInt(temp / 10);
+//     count++;
+// }
+
+// console.log(count);
+
+// --------------------------------------- Float value problem --------------------------------
+
+// console.log(0.1 + 0.2);
+
+//----------------------------------------- Math Object ---------------------------------------
+
+// let number = (Math.random() * 10);
+// let max = Math.min(10,20);
+// console.log(number);
+// console.log(Math.ceil(number));
+
+// let number = 11
+
+// console.log(Math.pow(number,4));
+
+console.log("hello" && false);
+console.log(100 || 200 || 0);
+console.log(0 ?? "hello"); // Nullish operator
---
+++ 04_Functions/function.js
@@ -0,0 +1,69 @@
+// Function Declaration
+
+// function greet(){
+//     console.log("Good morning");
+// }
+
+// greet();
+
+//  Function Expression
+
+// const greeting = function (){
+    // console.log("Good morning biji vakhat"); 
+    // return "good morning triji vakhat";  
+// }
+
+// console.log(greeting);
+// greeting();
+// console.log();
+
+// function greet(name){ //parameters
+//     console.log("Hello",name);
+// }
+
+// greet("Javascript"); // Arguments
+
+// ----------------------------------------------- Function Types -------------------------------------------------------
+
+/*
+1) WO A WO R
+2) W A WO R
+3) WO A W R
+4) W A W R
+*/
+// -------------------------------------------------------------------- 01
+// function add(){
+//     // Without Argument and without return
+//     console.log(10 + 20);
+// }
+
+// add();
+// -------------------------------------------------------------------- 02
+// function add1(a,b){
+//     // With Argument and without return
+//     console.log(a + b);
+// }
+
+// add1(10,20);
+// -------------------------------------------------------------------- 03
+// function add2(){
+//     // Without Argument and with return
+//     return 10 + 20;
+// }
+
+// console.log(add2());
+// -------------------------------------------------------------------- 04
+// function add3(a,b){
+//     // With Argument and with return
+//     return a + b;
+// }
+
+// console.log(add3(10,20));
+
+// ---------------------------------Default Parameter-----------------------------------
+
+// function add(a = 10,b = 0){
+//     console.log(a + b);
+// }
+
+// add();
