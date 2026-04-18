eecb7a80384f7572546d4e2c009b2ddccc98b831
TapanRoyal31 <tapanr3105@gmail.com>
2026-04-18 11:23:57 +0530

    arr slice and splice

---
+++ 05_Array/Array2.js
@@ -0,0 +1,27 @@
+// let arr = [10, 35, 67522];
+
+// console.log(arr.indexOf()); // return index
+// console.log(arr.includes()); // return true/false
+
+// console.log(arr.find((x) => x > 100)); // return the element
+// console.log(arr.findIndex((x) => x > 100)); // return the index of element
+
+// let newArr = arr.concat(["merge", "the", "arr"]); // return the merged array
+
+// console.log("original :",newArr); 
+// console.log("--------------------------------------------------------");
+
+// console.table(newArr); // print arr/obj in table format
+
+// console.log(newArr.slice(1,3)); // start index and ignore ending index
+// console.log("after slicce :",newArr);
+// console.log("--------------------------------------------------------");
+
+// console.log(newArr.splice(1,3)); // start index and last index
+// console.log("--------------------------------------------------------");
+
+// console.log("after splicce :",newArr);
+
+
+// Slice : don't modify original arr
+// Splice : modify original arr
