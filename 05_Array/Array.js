63644d670fd7890014fdcd77548b88ba93dbd905
TapanRoyal31 <tapanr3105@gmail.com>
2026-04-18 10:41:40 +0530

    array intro and task

---
+++ 05_Array/Array.js
@@ -0,0 +1,34 @@
+// const arr = [];
+
+// console.log(typeof arr);
+
+
+// arr[0] = "Heelo";
+// arr[1] = "log";
+// console.log(arr.length);
+
+// console.log(arr);
+
+// let newArr = ["hi","bye!!"];
+
+// let arrnew = arr.concat(newArr)
+// console.log(arrnew);
+
+// let arr = []
+// arr.push("Raj");
+// arr.push("simran")
+
+// arr.pop();
+
+// console.log(arr);
+
+// arr.unshift("simran");
+
+// console.log(arr);
+
+// arr.shift();
+
+// console.log(arr);
+
+
+
