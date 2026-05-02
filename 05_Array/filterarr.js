261fe5438cae6e939fd97d00a2a7b8db4f8ab006
TapanRoyal31 <tapanr3105@gmail.com>
2026-05-02 10:37:32 +0530

    filter task

--- 05_Array/filterarr.js
+++ 05_Array/filterarr.js
@@ -1,5 +1,12 @@
-let arr = [-2, 2, 3, 0, -5, 6, 2];
+// let arr = [-2, 2, 3, 0, -5, 6, 2];
 
-let newArr = arr.filter((num) => num == 2);
+// let newArr = arr.filter((num) => num == 2);
 
-console.log(newArr);
+// console.log(newArr);
+
+
+// const values = [0, "hello", false, "", 42, null, "world"];
+
+// let result = values.filter((value)=> value)
+
+// console.log(result);
