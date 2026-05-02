b1c371ecc8149eaccdb5880a76140487b1669a12
TapanRoyal31 <tapanr3105@gmail.com>
2026-05-02 11:13:45 +0530

    reduce

---
+++ 05_Array/Array3.js
@@ -0,0 +1,34 @@
+// const nums = [1, 2, 3, 4];
+
+// let result = nums.reduce((num1,num2) => num1 * num2,1);
+// console.log(result);
+
+// -------------------------------------------------------------------
+// const fruits = ["apple", "banana", "apple", "orange", "banana"];
+
+// let obj1 = fruits.reduce((obj,fruit) => {
+//     obj[fruit] = (obj[fruit] || 0) + 1;  
+//     return obj;
+// },{})
+
+// console.log(obj1);
+// ------------------------------------------------------------------- 
+
+// const arr = [[1, 2], [3, 4], [5]];
+
+// let newArr = arr.reduce((acc,value) => acc.concat(value));
+// console.log(newArr);
+
+// ------------------------------------------------------------------------
+/*
+const nums = [1, 2, 3, 4, 5];
+
+👉 Using ONLY reduce:
+
+filter even numbers
+double them
+sum result
+
+👉 Final: 12 (2→4, 4→8 → 4+8)
+
+*/
